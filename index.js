a1428b14684d23acba50cf2c3c2057cf96e0a535
Zak <zakattack26111@gmail.com>
2017-09-22 23:34:01 -1000

    fixed HTML append heiarchy

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let buildHtmlElements = (title, director, episode, producer, opening_crawl, release) => {
   let filmDiv = $('<div class="film"></div>');
-  let childrenElements = [[$('<div class="title"></div>'), title], [$('<div class="director"></div>'), director], [$('<div class="episode"></div>'), episode], [$('<div class="producer"></div>'), producer], [$('<div class="opening_crawl"></div>'), opening_crawl], [$('<div class="release"></div>'), release]];
-  childrenElements.map((currVal, index) => $('#films').append(filmDiv).append(currVal[0]).append(currVal[1]));
+  let childrenElements = [[$('<div class="title"></div>'), 'Title: ' + title], [$('<div class="director"></div>'), 'Director: ' + director], [$('<div class="episode"></div>'), 'Episode: ' + episode], [$('<div class="producer"></div>'), 'Producer: ' + producer], [$('<div class="opening_crawl"></div>'), 'Opening Crawl: ' + opening_crawl], [$('<div class="release"></div>'), 'Release: ' + release]];
+  childrenElements.map((currVal, index) => $('#films').append(filmDiv.append(currVal[0].append(currVal[1]))));
 }
 $('button').click(function(){$.get("https://swapi.co/api/films/", data => data.results.map(currVal => buildHtmlElements(currVal.title, currVal.director, currVal.episode_id, currVal.producer, currVal.opening_crawl, currVal.release_date)), 'JSON')});
